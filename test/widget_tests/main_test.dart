652d160ccbaa3134de100cf8847dea65a34b873f
Rathish <rathishrajendran9601@gmail.com>
2022-04-26 22:13:09 +0530

    Basic HomePage created

---
+++ test/widget_tests/main_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:interaction_forum/main.dart';
+import 'package:interaction_forum/home_page.dart';
+
+void main() {
+  testWidgets("Testing Leaf widget", (WidgetTester tester) async {
+    await tester.pumpWidget(Alum());
+
+    // Test: MaterialApp widget presence
+    expect(find.byType(MaterialApp), findsOneWidget);
+
+    // Test: HomePage widget presence
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+}
