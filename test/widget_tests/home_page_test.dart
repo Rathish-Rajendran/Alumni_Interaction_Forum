5b0dbb72e11e4d97e0d71bf91ee4356efee684ad
Rathish <rathishrajendran9601@gmail.com>
2022-04-27 11:12:41 +0530

    Home Page updated

--- test/widget_tests/home_page_test.dart
+++ test/widget_tests/home_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:interaction_forum/home_page.dart';
+import 'package:interaction_forum/view/pages/home_page.dart';
 
 void main() {
   testWidgets("Testing HomePage widget", (WidgetTester tester) async {
--- test/widget_tests/main_test.dart
+++ test/widget_tests/main_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:interaction_forum/main.dart';
-import 'package:interaction_forum/home_page.dart';
+import 'package:interaction_forum/view/pages/home_page.dart';
 
 void main() {
   testWidgets("Testing Leaf widget", (WidgetTester tester) async {
@@ -11,6 +11,6 @@ void main() {
     expect(find.byType(MaterialApp), findsOneWidget);
 
     // Test: HomePage widget presence
-    expect(find.byType(HomePage), findsOneWidget);
+    expect(find.byType(HomePage), findsOneWidget);  
   });
 }
